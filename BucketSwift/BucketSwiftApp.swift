fb7868b0a593cb72d514d3b8c6ede3acb9e11636
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-01-23 15:29:08 +0200

    Initial Commit

---
+++ BucketSwift/BucketSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketSwiftApp.swift
+//  BucketSwift
+//
+//  Created by Pat on 2023/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
