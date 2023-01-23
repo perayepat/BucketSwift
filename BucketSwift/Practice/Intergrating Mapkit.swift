f0ffd993bc2529e20ff218e1c235f80f0a669cac
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-01-23 16:33:22 +0200

    map Added

--- BucketSwift/Practice/Intergrating Mapkit.swift
+++ BucketSwift/Practice/Intergrating Mapkit.swift
@@ -1,9 +1,20 @@
-
+import MapKit
 import SwiftUI
 
 struct Intergrating_Mapkit: View {
+    
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12),span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Map(coordinateRegion: $mapRegion, annotationItems: Location.locations) { location in
+                MapMarker(coordinate: location.coordinate,tint: .green)
+            }
+                .ignoresSafeArea()
+            Color.red
+                .ignoresSafeArea()
+                .blendMode(.hue)
+        }
     }
 }
 
@@ -11,4 +22,17 @@ struct Intergrating_Mapkit_Previews: PreviewProvider {
     static var previews: some View {
         Intergrating_Mapkit()
     }
+}
+
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
+extension Location {
+    static let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
 }
